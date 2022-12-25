ddc47a8215ebd9f4b67388d11ca1c992c0acd547
Rodney Bejabeng <Rodneyb2@illinois.edu>
2022-12-25 00:42:07 -0800

    Done

--- calculator.js
+++ calculator.js
@@ -24,7 +24,7 @@ const form  = document.getElementById("TotalComp");
 // Base Salary Calculator
 document.querySelectorAll('.input-field').forEach(item => {
     item.addEventListener('keyup', event => {
-        BaseSalary.value = HourlySalary.value * Duration.value * HoursPerWeek.value;
+        BaseSalary.value = parseFloat(HourlySalary.value) * Duration.value * HoursPerWeek.value;
 
         let BaseSalaryValue = BaseSalary.value;
         return BaseSalaryValue;
@@ -48,3 +48,36 @@ item.addEventListener('keyup', event => {
     return TotalCompensatonValue;
 })
 });
+
+
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    validate();
+
+    let BaseSalary = HourlySalary.value * Duration.value * HoursPerWeek.value;
+
+    let MonthlySalary = parseFloat((4.29565217391 * HourlySalary.value * HoursPerWeek.value).toFixed(2));
+    console.log(MonthlySalary)
+
+    //Monthly Salary
+    document.getElementById("MonthlySalary").innerHTML = `$ ${MonthlySalary}`;
+};
+
+
+function validate() {
+    if (
+        HourlySalary.value === "" || Duration.value === "" || HoursPerWeek.value === "" || HousingStipend.value === "" || Bonus.value === ""
+    ) {
+
+        let alert = document.createElement("div");
+        alert.className = "btn red btn-large";
+        alert.innerHTML = `<span> Complete all fields </span>`;
+        alert.style.margin = ".5rem 35%";
+        form.parentNode.insertBefore(alert,form);
+
+        alert.onclick = () => alert.remove();
+
+        setTimeout(() => alert.remove(), "3000");
+    }
+}
