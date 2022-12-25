ac697f5f957aa46423ea5ec0d4407c72979dad50
Rodney Bejabeng <Rodneyb2@illinois.edu>
2022-12-24 23:29:46 -0800

    Calculator fully functional

--- calculator.js
+++ calculator.js
@@ -21,6 +21,7 @@ const form  = document.getElementById("TotalComp");
 
 //console.log(HourlySalary, Duration, HoursPerWeek, Housing, Bonus, TotalCompensaton);
 
+// Base Salary Calculator
 document.querySelectorAll('.input-field').forEach(item => {
     item.addEventListener('keyup', event => {
         BaseSalary.value = HourlySalary.value * Duration.value * HoursPerWeek.value;
@@ -29,6 +30,24 @@ document.querySelectorAll('.input-field').forEach(item => {
         return BaseSalaryValue;
     })
   });
+
+// Housing Calculator
+HousingStipend.addEventListener("keyup", () => {
+    Housing.value = HousingStipend.value * (Duration.value / 4);
+
+    let HousingValue = Housing.value;
+    return HousingValue;
+});
+
+// Total Compensation Calculator
+document.querySelectorAll('.output-field').forEach(item => {
+item.addEventListener('keyup', event => {
+    TotalCompensaton.value = parseInt(BaseSalary.value) + parseInt(HousingStipend.value) + parseInt(Bonus.value);
+
+    let TotalCompensatonValue = TotalCompensaton.value;
+    return TotalCompensatonValue;
+})
+});
 
 
 // HoursPerWeek.addEventListener("keyup", () => {
